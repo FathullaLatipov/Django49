319fe265276f2c213891d18bfea0fb6ed9ad1a89
FathullaLatipov <fathullaxonlatipov@gmail.com>
2024-03-20 19:33:55 +0500

    new changes

--- products/views.py
+++ products/views.py
@@ -3,12 +3,13 @@ from django.contrib.auth.views import LoginView
 from django.shortcuts import render, redirect
 
 from products.models import CategoryModel, ProductModel
-
+from products.forms import SearchForm
 
 def home_page(request):
     categories = CategoryModel.objects.all()
     products = ProductModel.objects.all()
-    context = {'categories': categories, 'products': products}
+    form = SearchForm
+    context = {'categories': categories, 'products': products, 'form': form}
     return render(request, template_name='index.html', context=context)
 
 
@@ -23,3 +24,26 @@ class MyLoginView(LoginView):
 def logout_view(request):
     logout(request)
     return redirect('home')
+
+
+def search(request):
+    if request.method == "POST":
+        get_product = request.POST.get('search_product')
+        try:
+            exact_product = ProductModel.objects.get(product_title__icontains=get_product)
+            return redirect(f'/products/{exact_product.id}')
+        except:
+            return redirect('/')
+
+
+def product_page(request, pk):
+    product = ProductModel.objects.get(id=pk)
+    context = {'product': product}
+    return render(request, 'product.html', context)
+
+
+def category_page(request, pk):
+    category = CategoryModel.objects.get(id=pk)
+    current_products = ProductModel.objects.filter(product_category=category)
+    context = {'product': current_products}
+    return render(request, 'category.html', context)
--- shop/urls.py
+++ shop/urls.py
@@ -4,13 +4,16 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
-from products.views import home_page, MyLoginView, logout_view
+from products.views import home_page, MyLoginView, logout_view, search, product_page, category_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_page, name='home'),
     path('login/', MyLoginView.as_view()),
-    path('logout/', logout_view)
+    path('logout/', logout_view),
+    path('search', search),
+    path('products/<int:pk>', product_page),
+    path('category/<int:pk>', category_page)
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
